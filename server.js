4a114371777328e7c7d6ec037885685437ff204e
abhisingh <developer3@qualityvector.in>
2021-01-06 17:10:13 +0530

    Twilio Authentication

--- server.js
+++ server.js
@@ -12,24 +12,23 @@ app.use(bodyParser.json());
 // Your Account Sid and Auth Token from twilio.com/console
 // and set the environment variables. See http://twil.io/secure
 const accountSid = 'ACcde02089100f0a483b76738a932c718b'; 
-const authToken = '4dbffeda3d02b3a1396ab0c40f0050c6'; 
+const authToken = '4dbffeda3d02b3a1396ab0c40f0050c6';  
 const client = require('twilio')(accountSid, authToken);
+
+client.messages 
+.create({ 
+   body: 'Hello! This is an editable text message. You are free to change it and write whatever you like.', 
+   from: 'whatsapp:+14155238886',       
+   to: 'whatsapp:+919870938538' 
+ }) 
+.then(message => console.log(message.sid)) 
+.done();
  
 
-  // Endpoints
+// Request Handling
 app.post('/logFile', (req, res) => { 
-    
-
    console.log(req);
-   client.messages 
-      .create({ 
-         body: 'Hello! This is an editable text message. You are free to change it and write whatever you like.', 
-         from: 'whatsapp:+14155238886',       
-         to: 'whatsapp:+919870938538' 
-       }) 
-      .then(message => console.log(message.sid)) 
-      .done();
- });
+});
 
 app.listen(8080,()=> {
 console.log("Server is running on 8080");
