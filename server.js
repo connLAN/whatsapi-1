7a0b7e94663912e565104d0d58444cbb1cba0726
abhisingh <developer3@qualityvector.in>
2021-01-08 11:17:41 +0530

    Request

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-// const accountSid = "ACcde02089100f0a483b76738a932c718b";
-// const authToken = "a9e31f00a25d38e90a455a4e2b298459";
-// const client = require("twilio")(accountSid, authToken);
+const accountSid = "ACcde02089100f0a483b76738a932c718b";
+const authToken = "a9e31f00a25d38e90a455a4e2b298459";
+const client = require("twilio")(accountSid, authToken);
 
 //const client = require('twilio')();
 
